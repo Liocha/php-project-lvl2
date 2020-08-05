5768bb41fb81cd6ddbcf06306d9f69d047f0653a
Alex <dev@liocha.ru>
2020-08-06 00:30:49 +0300

    added recursive comparison

--- src/Differ.php
+++ src/Differ.php
@@ -25,9 +25,8 @@ function run()
     $format = $args["--format"];
     $path_to_first_file = $args["<firstFile>"];
     $path_to_second_file = $args["<secondFile>"];
-    dump($format);
 
-    $diff = genDiff($path_to_first_file, $path_to_second_file, $format);
+    $diff = "{\n" . genDiff($path_to_first_file, $path_to_second_file, $format) . "}\n";
 
     echo ($diff);
 }
@@ -39,19 +38,82 @@ function genDiff($path_to_first_file, $path_to_second_file, $format = null)
     $data_from_second_file = get_content($path_to_second_file);
 
     if ($format === 'yml') {
-        $first_file_assoc = Yaml::parse($data_from_first_file, Yaml::PARSE_OBJECT_FOR_MAP);
-        $second_file_assoc = Yaml::parse($data_from_second_file, Yaml::PARSE_OBJECT_FOR_MAP);
+        $first_file_assoc = Yaml::parse($data_from_first_file);
+        $second_file_assoc = Yaml::parse($data_from_second_file);
     } elseif ($format === 'json') {
         $first_file_assoc = json_decode($data_from_first_file, true);
         $second_file_assoc = json_decode($data_from_second_file, true);
     }
 
-
-    $resault_tree  = create_resault_tree($first_file_assoc);
-
+    $resault_tree  = create_tree($first_file_assoc);
     $updated_resault_tree  = update_resault_tree($resault_tree, $second_file_assoc);
 
     return  get_resault($updated_resault_tree);
+}
+
+function update_resault_tree($resault, $json_file_assoc, $deep = 1)
+{
+    foreach ($json_file_assoc as $key => $val) {
+        if (isset($resault[$key])) {
+            if (is_array($val) && count($resault[$key]['children']) > 0) {
+                $resault[$key] = array_merge(
+                    $resault[$key],
+                    ['meta' => ['eql', $deep]],
+                    ['children' =>  update_resault_tree(
+                        $resault[$key]['children'],
+                        $val,
+                        $deep + 1
+                    )]
+                );
+            }
+
+            if (!is_array($val) && count($resault[$key]['children']) > 0) {
+                $resault[$key] = array_merge($resault[$key], ['meta' => ['mod', $deep]], ['new_val' =>  $val]);
+            }
+
+            if (is_array($val) && count($resault[$key]['children']) === 0) {
+                $resault[$key] = array_merge(
+                    $resault[$key],
+                    ['meta' => ['mod', $deep]],
+                    ['children' =>  update_resault_tree([], $val, $deep + 1)]
+                );
+            }
+
+            if (!is_array($val) && count($resault[$key]['children']) === 0) {
+                $resault[$key] = $resault[$key]['old_val'] === $val ?
+                    array_merge($resault[$key], ['meta' => ['eql', $deep]], ['new_val' =>  $val]) :
+                    array_merge($resault[$key], ['meta' => ['mod', $deep]], ['new_val' =>  $val]);
+            }
+        } else {
+            if (is_array($val)) {
+                $resault[$key] = ['meta' => ['new', $deep],  'children' => update_resault_tree(
+                    [],
+                    $val,
+                    $deep + 1
+                )];
+            } else {
+                $resault[$key] = ['meta' => ['new', $deep], 'new_val' => $val, 'children' => []];
+            };
+        }
+    }
+
+    return $resault;
+}
+
+
+function create_tree($assoc, $deep = 1)
+{
+    $resault = [];
+
+    foreach ($assoc as $key => $val) {
+        if (is_array($val)) {
+            $resault[$key] = ['meta' => ['old', $deep],  'children' => create_tree($val, $deep + 1)];
+        } else {
+            $resault[$key] = ['meta' => ['old', $deep], 'old_val' => $val, 'children' => []];
+        };
+    }
+
+    return $resault;
 }
 
 function is_absolute_path($path)
@@ -72,66 +134,75 @@ function get_content($path_to_file)
     return $data;
 }
 
-function create_resault_tree($json_file_assoc)
+function get_resault($resault_tree, $mod = true, $resault = '')
 {
-    $resault = [];
-    foreach ($json_file_assoc as $key => $val) {
-        $resault[$key] = [false, $val, null];
-    }
 
-    return $resault;
-}
-
-function update_resault_tree($resault, $json_file_assoc)
-{
-    foreach ($json_file_assoc as $key => $val) {
-        if (isset($resault[$key])) {
-            [, $old_val] =  $resault[$key];
-
-            if ($old_val === $val) {
-                $resault[$key] = [null, $old_val, null];
-            } else {
-                $new_val = $val;
-                $resault[$key] = [true, $old_val, $new_val];
-            }
-        } else {
-            $resault[$key] = [true, $val, null];
-        }
-    }
-
-    return $resault;
-}
-
-function get_resault($resault_tree)
-{
-    $resault = '';
-    $resault = $resault . "\n" . "{" . "\n";
     foreach ($resault_tree as $key => $val) {
-        [$modified, $old_val, $new_val] = $val;
-
-        if (gettype($old_val) === 'boolean') {
-            $old_val = $old_val ? 'true' : 'false';
+        if (count($val['children']) > 0 &&  $val['meta'][0] !== 'mod') {
+            $resault  = $resault . get_ident($val['meta'][1]) . get_sign($val['meta'][0]) .
+                $key . ": {\n" . get_resault($val['children'], false) . get_ident($val['meta'][1]) . "  }\n";
+        } elseif (count($val['children']) > 0) {
+            $resault  = $resault . get_ident($val['meta'][1]) . get_sign($val['meta'][0]) .
+                $key . ": {\n" . get_resault($val['children']) . get_ident($val['meta'][1])  . "  }\n";
+        } else {
+            $resault = $resault . node_to_string($key, $val, $mod);
         }
-
-        if (gettype($new_val) === 'boolean') {
-            $new_val = $new_val ? 'true' : 'false';
-        }
-
-        if ($modified === null) {
-            $resault = $resault . "    {$key}: {$old_val}\n";
-            continue;
-        }
-
-        if ($modified === false) {
-            $resault = $resault . "  - {$key}: {$old_val}\n";
-            continue;
-        }
-
-        $resault = $new_val === null ?
-            $resault . "  + {$key}: {$old_val}\n" :
-            $resault . "  - {$key}: {$old_val}\n  + {$key}: {$new_val}\n";
     }
-    $resault = $resault . "}" . "\n";
 
+    return $resault;
+}
+
+function node_to_string($name, $node, $mod)
+{
+    [$meta_sign, $meta_deep] = $node['meta'];
+
+
+
+    $ident = get_ident($meta_deep);
+    $sign =  $mod ? get_sign($meta_sign) : '  ';
+    $tmp =  $meta_sign === 'eql' ? 'old_val' : "{$meta_sign}_val";
+
+    if ($meta_sign === 'mod') {
+        $let  = "{$ident}- {$name}: " . fix_bol_val($node['old_val']) . "\n{$ident}+ {$name}: " .
+            fix_bol_val($node['new_val']) . "\n";
+    } else {
+        $let  = "{$ident}{$sign}{$name}: " . fix_bol_val($node[$tmp]) . "\n";
+    }
+    return $let;
+}
+
+function fix_bol_val($val)
+{
+    if (gettype($val) === 'boolean') {
+        $val = $val ? 'true' : 'false';
+    }
+    return $val;
+}
+
+
+function get_sign($meta_data)
+{
+    switch ($meta_data) {
+        case 'eql':
+            return '  ';
+        case 'old':
+            return '- ';
+        case 'new':
+            return '+ ';
+        case 'mod':
+            return '+ ';
+        default:
+            throw new \Exception('Unknown meta data ' . $meta_data);
+    }
+}
+
+function get_ident($deep)
+{
+    $base = '  ';
+    $resault = '';
+    while ($deep * 2 > 0) {
+        $resault = $resault . $base;
+        $deep -= 1;
+    }
     return $resault;
 }
--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -9,18 +9,43 @@ use function Differ\Differ\genDiff;
 class GenDiffTest extends TestCase
 {
     private $expected;
+    private $expected_2;
 
     public function setUp(): void
     {
         $this->expected = <<<'EXP'
-
-        {
             host: hexlet.io
           - timeout: 50
           + timeout: 20
           - proxy: 123.234.53.22
           + verbose: true
-        }
+        
+        EXP;
+
+        $this->expected_2 = <<<'EXP'
+            common: {
+              setting1: Value 1
+              setting2: 200
+              setting3: true
+            - setting6: {
+                key: value
+              }
+              setting4: blah blah
+            + setting5: {
+                key5: value5
+              }
+            }
+            group1: {
+            - baz: bas
+            + baz: bars
+              foo: bar
+            }
+          - group2: {
+              abc: 12345
+            }
+          + group3: {
+              fee: 100500
+            }
         
         EXP;
     }
@@ -30,9 +55,19 @@ class GenDiffTest extends TestCase
         $this->assertSame($this->expected, genDiff('./tests/fixtures/before.json', './tests/fixtures/after.json', 'json'));
     }
 
+    public function testGenDiffByJsonTwo()
+    {
+        $this->assertSame($this->expected_2, genDiff('./tests/fixtures/before2.json', './tests/fixtures/after2.json', 'json'));
+    }
+
 
     public function testGenDiffByYaml()
     {
         $this->assertSame($this->expected, genDiff('./tests/fixtures/before.yml', './tests/fixtures/after.yml', 'yml'));
+    }
+
+    public function testGenDiffByYamlTwo()
+    {
+        $this->assertSame($this->expected_2, genDiff('./tests/fixtures/before2.yml', './tests/fixtures/after2.yml', 'yml'));
     }
 }
