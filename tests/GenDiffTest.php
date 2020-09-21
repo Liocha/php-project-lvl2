58c6672cf5618027d5049b6dc379bb7e50db2258
Alex <dev@liocha.ru>
2020-09-21 19:11:30 +0300

    refactoring

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -9,7 +9,7 @@ use function Differ\Differ\genDiff;
 class GenDiffTest extends TestCase
 {
 
-    protected $pathToFixtures = "./tests/fixtures/";
+    protected $pathToFixtures = __DIR__ . "/fixtures/";
 
     /**
      * @dataProvider additionProvider
@@ -17,28 +17,22 @@ class GenDiffTest extends TestCase
 
     public function testGenDiff($firstPath, $secondPath, $format, $expected)
     {
-        $this->assertEquals($expected, genDiff($firstPath, $secondPath, $format));
+        $resault =  file_get_contents($this->pathToFixtures . $expected);
+        $this->assertEquals(
+            $resault,
+            genDiff($this->pathToFixtures . $firstPath, $this->pathToFixtures . $secondPath, $format)
+        );
     }
 
     public function additionProvider()
     {
-        $expectedPretty = file_get_contents($this->pathToFixtures . 'pretty.txt');
-        $expectedPlain = file_get_contents($this->pathToFixtures . 'plain.txt');
-        $expectedJson = file_get_contents($this->pathToFixtures . 'json.txt');
-
-        $beforeJson = $this->pathToFixtures . 'before2.json';
-        $afterJson = $this->pathToFixtures . 'after2.json';
-
-        $beforeYml = $this->pathToFixtures . 'before2.json';
-        $afterYml = $this->pathToFixtures . 'after2.json';
-
         return [
-          [$beforeJson, $afterJson, 'pretty', $expectedPretty],
-          [$beforeJson, $afterJson, 'plain', $expectedPlain],
-          [$beforeJson, $afterJson, 'json', $expectedJson],
-          [$beforeYml, $afterYml, 'pretty', $expectedPretty],
-          [$beforeYml, $afterYml, 'plain', $expectedPlain],
-          [$beforeYml, $afterYml, 'json', $expectedJson]
+            ['before2.json', 'after2.json', 'pretty', 'pretty.txt'],
+            ['before2.json', 'after2.json', 'plain', 'plain.txt'],
+            ['before2.json', 'after2.json', 'json', 'json.txt'],
+            ['before2.yml', 'after2.yml', 'pretty', 'pretty.txt'],
+            ['before2.yml', 'after2.yml', 'plain', 'plain.txt'],
+            ['before2.yml', 'after2.yml', 'json', 'json.txt']
         ];
     }
 }
